ca15b5abe426eddc62e21250af565a3a4a42bdf8
liuyingyi <abrahamliu1986@gmail.com>
2022-01-11 20:28:21 +0900

    updated the rotational center input formatting in the control file

--- SourceCode/InputFiles.f90
+++ SourceCode/InputFiles.f90
@@ -98,7 +98,7 @@
         
         READ(1,*) 
         READ(1,*) 
-        READ(1,'(26x,3f12.3)')      (XR(I), I=1,3)
+        READ(1,'(26x,3f7.3)')      (XR(I), I=1,3)
         WRITE(9,*) "The rotation center is input as (please confirm if it is correct):"
         WRITE(9,'(3f12.3)') (XR(I), I=1,3)
         READ(1,'(26x,f30.15)')     REFL
